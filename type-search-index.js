ee6b1625b27c772ad56178acf9ac877372b19dfb
Your Name <you@example.com>
2024-05-24 01:04:04 +0600

    trying first java doc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.splash_learn","l":"UserProfile.Builder"},{"p":"com.example.splash_learn","l":"UserProfile"}];updateSearchResults();
