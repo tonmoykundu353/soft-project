00d5bf6d2494750c1fb243c453509d833d7c3b06
Your Name <you@example.com>
2024-05-24 09:42:41 +0600

    rework on javadoc

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.splash_learn","l":"UserProfile.Builder"},{"p":"com.example.splash_learn","l":"UserProfile"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.splash_learn","l":"RegStudAtEventInfo"}];updateSearchResults();
